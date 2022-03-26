4fe8d693bb1cbfa36ed6e58618e948549e92db71
bernardopiane <bernardopiane@gmail.com>
2022-03-26 13:28:53 -0300

    Box component created

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function Box() {
-  return (
-    <div>Box</div>
-  )
+
+const MyBox = styled.div`
+    padding: 40px;
+    background-color: #ea916b;
+    max-width: 780px;
+    border-radius: 10px;
+    color: white;
+    font-size: 15px;
+    line-height: 24px;
+    font-weight: 300;
+`;
+
+export default function Box({ children, ...props }) {
+    return (
+        <MyBox>{children}</MyBox>
+    )
 }
